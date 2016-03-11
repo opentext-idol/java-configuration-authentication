7b90bfc3a3832b5900bc460e348d957ae62f4f19
Ivo Miller <ivo.miller@hpe.com>
2016-03-11 15:20:23 +0000

    Add role information to community principal [rev. alex.scown]

--- src/main/java/com/hp/autonomy/frontend/configuration/authentication/CommunityPrincipal.java
+++ src/main/java/com/hp/autonomy/frontend/configuration/authentication/CommunityPrincipal.java
@@ -8,15 +8,23 @@ package com.hp.autonomy.frontend.configuration.authentication;
 import lombok.Data;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class CommunityPrincipal implements Principal {
     private final long id;
     private final String username;
+    @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
+    private final List<String> roles;
     private final String securityInfo;
 
     @Override
     public String getName() {
         return username;
+    }
+
+    public List<String> getRoles() {
+        return new ArrayList<>(roles);
     }
 }
